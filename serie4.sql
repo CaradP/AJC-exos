3cb2905e82f03e0874649bc3e311690e82ce474d
root <root@ip-172-31-31-216.us-east-2.compute.internal>
2024-01-23 17:39:51 +0000

    First commit, premier exo avec seulement les consignes

---
+++ serie4.sql
@@ -0,0 +1,24 @@
+-- Série 4 : (Jointure)
+-- 1. Ecrivez une requête pour afficher le nom, le numéro de département et le département de
+-- tous les employés.
+-- 2. Créez une liste unique de tous les postes du département 30.
+-- 3. Ecrivez une requête pour afficher le nom, le département et la localisation de tous les
+-- employés qui touchent une commission.
+-- 4. Affichez le nom et le nom du département pour tous les employés dont le nom contient la
+-- lettre A.
+-- 5. Ecrivez une requête pour afficher le nom, le poste, le numéro de département et le
+-- département de tous les employés basés à Seattle.
+-- 6. Affichez le nom et le matricule des employés et de leur manager. Nommez les colonnes
+-- Employee, Emp#, Manager, et Mgr#, respectivement.
+-- 7. Modifiez la requête précédent[SA1]e pour afficher tous les employés, y compris King n'ayant
+-- pas de manager.
+-- 8. Créez une requête pour afficher le numéro de département et le nom de tous les employés
+-- qui travaillent dans le même département qu'un autre employé. Donnez à chaque colonne
+-- un nom approprié.
+-- 9. Affichez la structure de la table JOBS. Créez une requête pour afficher le nom, le poste, le
+-- département, le salaire et l’intervalle associé de tous les employés.
+-- 10. Créez une requête pour afficher le nom et la date d'embauche de tous les employés arrivés
+-- après l'employé dont le mail est « JKING ».
+-- 11. Affichez les noms et date d'embauche des employés et de leur manager, pour tous les
+-- employés ayant été embauchés avant leur manager. Nommez les colonnes Employee, Emp
+-- #Hiredate, Manager et Mgr Hiredate.
